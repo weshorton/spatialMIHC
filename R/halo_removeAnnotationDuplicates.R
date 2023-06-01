6e855ceb2eedbc937baf206a84dd90be81cd0950
Wes Horton <hortowe@rnb11642.ohsu.edu>
2023-06-01 15:23:22 -0700

    Moved original removeAnnotationDuplicates to halo_ prefix (V's project). Update removeAnnotationDuplicates to try and be more robust in ability to handle different gating strategies. Seems somewhat fragile and can be improved, but works for HTAN project

---
+++ R/halo_removeAnnotationDuplicates.R
@@ -0,0 +1,96 @@
+halo_removeAnnotationDuplicates <- function(df, idCol_v = "OBJECTID", classCol_v = "class", mainClass_v = "Myeloid cells") {
+  #' Remove Annotation Duplicates
+  #' @description It's possible for the intensity and combination of markers in certain cells to cause that cell to be
+  #' assigned multiple labels. Must pick one. 
+  #' @param df clean data.frame output by annotations script
+  #' @param idCol_v column that contains the cell IDs
+  #' @param classCol_v column that contains the cell type annotations
+  #' @param mainClass_v so far it appears that all duplicate IDs belong to one main class and an alternate class. 
+  #' Not sure about this part entirely. Will this always be the case, or can there be multiple two-class combinations?
+  #' @details Take the cleaned, annotated data and search for duplicated cell IDs. Grab the cell annotations for these cells
+  #' and split them. One annotation will be kept for subsequent analysis, the other will be removed and recorded.
+  #' @return list of two data.frames: (1) "clean" contains the input data.frame with duplicates removed.
+  #' (2) "dup" contains the duplicate IDs and their classes.
+  #' @export
+  
+  ### Find Duplicated IDs
+  dupIDs_df <- as.data.frame(table(df[[idCol_v]]))
+  dupIDs_df <- dupIDs_df[dupIDs_df$Freq > 1,]
+  dupIDs_v <- unique(dupIDs_df$Var1)
+  
+  ### Subset input data for these
+  dupData_df <- df[df[[idCol_v]] %in% dupIDs_v,]
+  
+  ### Get identities of duplicated cells
+  dupCalls_v <- dupData_df[[classCol_v]]
+  print(table(dupCalls_v))
+  
+  ### Split the duplicated IDs into two equal data.frames. Merge together to compare
+  ### Ideally one data.frame will have all one cell type and the other will be a mix
+  dupData_df$which <- 1
+  for (d_v in dupIDs_v) dupData_df[dupData_df[[idCol_v]] == d_v, "which"] <- c(1,2)
+  dup1_df <- dupData_df[dupData_df$which == 1,]; uniq1_v <- unique(dup1_df[[classCol_v]])
+  dup2_df <- dupData_df[dupData_df$which == 2,]; uniq2_v <- unique(dup2_df[[classCol_v]])
+  dupCompareIDs_df <- merge(dup1_df[,c(idCol_v, classCol_v)], dup2_df[,c(idCol_v, classCol_v)], by = idCol_v, sort = F)
+  
+  ### Check dimensions
+  if (nrow(dup1_df) != nrow(dup2_df)) stop("The two data.frames created by splitting the duplicate IDs are not the same size.\n")
+  
+  ### Determine which, if any, of the above has the unique class and also check if it's the specified main class.
+  if (length(uniq1_v) == 1 & length(uniq2_v) == 1) {
+    
+    cat("Both sets of duplicate IDs have a single class (i.e. 1:1 class mapping)\n")
+    if (uniq1_v == mainClass_v) {
+      cat(sprintf("First data.frame matches main class %s.\n", mainClass_v))
+      out_df <- dup1_df; other_df <- dup2_df
+      outClass_v <- uniq1_v
+    } else if (uniq2_v == mainClass_v) {
+      cat(sprintf("Second data.frame matches main class %s.\n", mainClass_v))
+      out_df <- dup2_df; other_df <- dup1_df
+      outClass_v <- uniq2_v
+    } else {
+      warning(sprintf("Neither data.frame matches main class %s.\nUsing first data.frame class %s\n", mainClass_v, uniq1_v))
+      outClass_v <- uniq1_v
+    }
+    
+  } else if (length(uniq1_v) == 1 & length(uniq2_v) > 1) {
+    
+    cat("First data.frame has single class\n")
+    if (uniq1_v == mainClass_v) {
+      cat(sprintf("Selected class %s matches main class provided (%s).\n", uniq1_v, mainClass_v))
+    } else {
+      warning(sprintf("Selected class %s doesn't match main class provided (%s).\n", uniq1_v, mainClass_v))
+    } # fi
+    
+    out_df <- dup1_df; other_df <- dup2_df
+    outClass_v <- uniq1_v
+    
+  } else if (length(uniq1_v) > 1 & length(uniq2_v) == 1) {
+    
+    cat("Second data.frame has single class\n")
+    if (uniq2_v == mainClass_v) {
+      cat(sprintf("Selected class %s matches main class provided (%s).\n", uniq2_v, mainClass_v))
+    } else {
+      warning(sprintf("Selected class %s doesn't match main class provided (%s).\n", uniq2_v, mainClass_v))
+    } # fi
+    
+    out_df <- dup2_df; other_df <- dup1_df
+    outClass_v <- uniq2_v
+    
+  } else {
+    
+    warning("Both data.frames have >1 unique class. Keeping df1 and assigning df2 to 'other'. Be sure to check output\n")
+    out_df <- dup1_df; other_df <- dup2_df
+    outClass_v <- uniq1_v
+    
+  } # fi
+  
+  ### Remove the set we don't want
+  df <- df[!(df[[idCol_v]] %in% dupData_df[[idCol_v]] & df[[classCol_v]] == outClass_v),]
+  
+  ### Outputs
+  out_lsdf <- list("clean" = df, "dup" = dupCompareIDs_df)
+  return(out_lsdf)
+  cat("\n")
+  
+} # removeAnnotationDuplicates
--- R/removeAnnotationDuplicates.R
+++ R/removeAnnotationDuplicates.R
@@ -1,22 +1,26 @@
-removeAnnotationDuplicates <- function(df, idCol_v = "OBJECTID", classCol_v = "class", mainClass_v = "Myeloid cells") {
+removeAnnotationDuplicates <- function(df, idCol_v = "OBJECTID", classCol_v = "class", keep_v = 2) {
   #' Remove Annotation Duplicates
   #' @description It's possible for the intensity and combination of markers in certain cells to cause that cell to be
   #' assigned multiple labels. Must pick one. 
   #' @param df clean data.frame output by annotations script
   #' @param idCol_v column that contains the cell IDs
   #' @param classCol_v column that contains the cell type annotations
-  #' @param mainClass_v so far it appears that all duplicate IDs belong to one main class and an alternate class. 
-  #' Not sure about this part entirely. Will this always be the case, or can there be multiple two-class combinations?
+  #' @param keep_v value indicating which class to keep. Acceptable values are "first" for 1st class, "last" for last class, 
+  #' or a numeric value (e.g. 2) indicating which to take.
   #' @details Take the cleaned, annotated data and search for duplicated cell IDs. Grab the cell annotations for these cells
   #' and split them. One annotation will be kept for subsequent analysis, the other will be removed and recorded.
-  #' @return list of two data.frames: (1) "clean" contains the input data.frame with duplicates removed.
-  #' (2) "dup" contains the duplicate IDs and their classes.
+  #' @return list of three data.frames: (1) "clean" contains the input data.frame with duplicates removed.
+  #' (2) "dup" contains the duplicate IDs and their classes. (3) is a summary of 2.
   #' @export
+  
+  ###
+  ### Wrangle Duplicate IDs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ###
   
   ### Find Duplicated IDs
   dupIDs_df <- as.data.frame(table(df[[idCol_v]]))
   dupIDs_df <- dupIDs_df[dupIDs_df$Freq > 1,]
-  dupIDs_v <- unique(dupIDs_df$Var1)
+  dupIDs_v <- unique(as.numeric(as.character(dupIDs_df$Var1)))
   
   ### Subset input data for these
   dupData_df <- df[df[[idCol_v]] %in% dupIDs_v,]
@@ -25,71 +29,70 @@ removeAnnotationDuplicates <- function(df, idCol_v = "OBJECTID", classCol_v = "c
   dupCalls_v <- dupData_df[[classCol_v]]
   print(table(dupCalls_v))
   
-  ### Split the duplicated IDs into two equal data.frames. Merge together to compare
-  ### Ideally one data.frame will have all one cell type and the other will be a mix
+  ###
+  ### Something ~~~~~~~~~~~~~
+  ###
+  
+  ### New column to identify which occurrence of ID we have
   dupData_df$which <- 1
-  for (d_v in dupIDs_v) dupData_df[dupData_df[[idCol_v]] == d_v, "which"] <- c(1,2)
-  dup1_df <- dupData_df[dupData_df$which == 1,]; uniq1_v <- unique(dup1_df[[classCol_v]])
-  dup2_df <- dupData_df[dupData_df$which == 2,]; uniq2_v <- unique(dup2_df[[classCol_v]])
-  dupCompareIDs_df <- merge(dup1_df[,c(idCol_v, classCol_v)], dup2_df[,c(idCol_v, classCol_v)], by = idCol_v, sort = F)
+  for (i in 1:nrow(dupIDs_df)) dupData_df[dupData_df[[idCol_v]] == dupIDs_df$Var1[i], "which"] <- 1:dupIDs_df$Freq[i]
   
-  ### Check dimensions
-  if (nrow(dup1_df) != nrow(dup2_df)) stop("The two data.frames created by splitting the duplicate IDs are not the same size.\n")
+  ### Dataframe for each ocurrence
+  which_v <- unique(dupData_df$which)
+  if (length(which_v) > 3) warning("At least some cells have >3 classes. Have only tested this for up to 3 classes.\n")
+  lsdf <- uniq_lsv <- list()
+  for (w_v in which_v) {
+    curr_df <- dupData_df[dupData_df$which == w_v,]
+    currUniq_v <- unique(curr_df[[classCol_v]])
+    lsdf[[as.character(w_v)]] <- curr_df
+    uniq_lsv[[as.character(w_v)]] <- currUniq_v
+  }
   
-  ### Determine which, if any, of the above has the unique class and also check if it's the specified main class.
-  if (length(uniq1_v) == 1 & length(uniq2_v) == 1) {
+  ### Merge
+  dupCompareIDs_df <- mergeDTs(data_lsdt = lsdf, mergeCol_v = idCol_v, keepCol_v = classCol_v, sort = F)
+  
+  ### Summarize
+  dupSummary_dt <- as.data.table(table(apply(dupCompareIDs_df[,mget(names(lsdf))], 1, function(x) paste(x, collapse = '-_-'))))
+  
+  ###
+  ### Grab Correct Class
+  ###
+  
+  if (keep_v == "first") {
     
-    cat("Both sets of duplicate IDs have a single class (i.e. 1:1 class mapping)\n")
-    if (uniq1_v == mainClass_v) {
-      cat(sprintf("First data.frame matches main class %s.\n", mainClass_v))
-      out_df <- dup1_df; other_df <- dup2_df
-      outClass_v <- uniq1_v
-    } else if (uniq2_v == mainClass_v) {
-      cat(sprintf("Second data.frame matches main class %s.\n", mainClass_v))
-      out_df <- dup2_df; other_df <- dup1_df
-      outClass_v <- uniq2_v
-    } else {
-      warning(sprintf("Neither data.frame matches main class %s.\nUsing first data.frame class %s\n", mainClass_v, uniq1_v))
-      outClass_v <- uniq1_v
-    }
+    out_df <- lsdf[[1]]
+    other_df <- do.call(rbind, lsdf[-1])
     
-  } else if (length(uniq1_v) == 1 & length(uniq2_v) > 1) {
+  } else if (keep_v == "last") {
     
-    cat("First data.frame has single class\n")
-    if (uniq1_v == mainClass_v) {
-      cat(sprintf("Selected class %s matches main class provided (%s).\n", uniq1_v, mainClass_v))
-    } else {
-      warning(sprintf("Selected class %s doesn't match main class provided (%s).\n", uniq1_v, mainClass_v))
-    } # fi
+    warning("Attempting to grab the last ID for all. This hasn't been thoroughly tested.")
+    last_v <- length(which_v)
+    out_df <- lsdf[[last_v]]
+    counter_v <- 1
+    while (nrow(out_df) < nrow(lsdf[[1]])) {
+      missing_v <- setdiff(lsdf[[(last_v-counter_v)]][[idCol_v]], out_df[[idCol_v]])
+      out_df <- rbind(out_df, lsdf[[(last_v-counter_v)]][lsdf[[(last_v-counter_v)]][[idCol_v]] %in% missing_v,])
+      lsdf[[(last_v-counter_v)]] <- lsdf[[(last_v-counter_v)]][!(lsdf[[(last_v-counter_v)]][[idCol_v]] %in% missing_v),]
+      counter_v <- counter_v + 1
+    } # while
     
-    out_df <- dup1_df; other_df <- dup2_df
-    outClass_v <- uniq1_v
-    
-  } else if (length(uniq1_v) > 1 & length(uniq2_v) == 1) {
-    
-    cat("Second data.frame has single class\n")
-    if (uniq2_v == mainClass_v) {
-      cat(sprintf("Selected class %s matches main class provided (%s).\n", uniq2_v, mainClass_v))
-    } else {
-      warning(sprintf("Selected class %s doesn't match main class provided (%s).\n", uniq2_v, mainClass_v))
-    } # fi
-    
-    out_df <- dup2_df; other_df <- dup1_df
-    outClass_v <- uniq2_v
+    other_df <- do.call(rbind, lsdf[-last_v])
     
   } else {
     
-    warning("Both data.frames have >1 unique class. Keeping df1 and assigning df2 to 'other'. Be sure to check output\n")
-    out_df <- dup1_df; other_df <- dup2_df
-    outClass_v <- uniq1_v
+    out_df <- lsdf[[keep_v]]
+    other_df <- do.call(rbind, lsdf[-keep_v])
     
   } # fi
   
-  ### Remove the set we don't want
-  df <- df[!(df[[idCol_v]] %in% dupData_df[[idCol_v]] & df[[classCol_v]] == outClass_v),]
+  ###
+  ### Remove from main
+  ###
+ 
+  df <- df[!other_df, on=cols_v]
   
   ### Outputs
-  out_lsdf <- list("clean" = df, "dup" = dupCompareIDs_df)
+  out_lsdf <- list("clean" = df, "dup" = dupCompareIDs_df, "summary" = dupSummary_dt)
   return(out_lsdf)
   cat("\n")
   
