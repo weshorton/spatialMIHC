de00b9836772a0bec6b82f68e7dc1d05372e0bd8
Wes Horton <hortowe@rnb11642.ohsu.edu>
2023-06-02 09:36:52 -0700

    Updated defaults for reformatForSeurat to be better for our project. Small update to help info as well. TODO - can we create a rule that will always find the right expression and meta columns, or will we have to determine per project?

--- R/reformatForSeurat.R
+++ R/reformatForSeurat.R
@@ -1,13 +1,17 @@
-reformatForSeurat <- function(df, idCol_v = "OBJECTID", coordCols_v = c("OBJECTID", "XCENTRE", "YCENTRE"), exprCols_v, metaCols_v) {
+reformatForSeurat <- function(df, idCol_v = "ObjectNumber", 
+                              coordCols_v = c("OBJECTID", "Location_Center_X", "Location_Center_Y"), 
+                              exprCols_v, metaCols_v) {
   #' Reformat data for Seurat
   #' @description Reformat cleaned data.frames output from annotation scripts so they can be converted to Seurat objects
   #' @param df clean data.frame output by annotations script
   #' @param idCol_v column that contains the cell IDs. Used to make sure all data.frames are in same order.
   #' @param coordCols_v character vector of the 3 columns that will be extracted in order to form the spatial component. 
-  #' These must represent the cell ID and the X/Y coordinates for the cell's center IN THAT ORDER. Default is c("OBJECTID", "XCENTRE", "YCENTRE")
+  #' These must represent the cell ID (idCol_v) and the X/Y coordinates for the cell's center IN THAT ORDER. 
+  #' Default is c("ObjectNumber", "XCENTRE", "YCENTRE")
   #' @param exprCols_v character vector of the marker intensity columns. (TRY TO MAKE THIS AUTOMATIC)
   #' @param metaCols_v character vector of any columns to be added to the Seurat object metadata. 
-  #' Needs to include at a minimum the annotated cell class column and the cell ID column. Can include any other metadata that you may want to visualize downstream.
+  #' Needs to include at a minimum the annotated cell class column and the cell ID column. 
+  #' Can include any other metadata that you may want to visualize downstream. (_func columns are common)
   #' @details To construct a Seurat object, we need:
   #' A data.frame of x/y coordinates. This is used for plotting and is analagous to the XY coordinates generated from a UMAP call
   #' A data.frame of marker intensities. This is analogous to gene expression data in a normal scRNAseq Seurat object
