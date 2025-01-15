57e90c62d575df7c41f2d9e3a1507ea2f25cbad3
weshorton <wrhorton592@gmail.com>
2025-01-15 15:42:01 -0800

    Add verbose and some tocs to cluster sweep

--- R/kClusterSweep.R
+++ R/kClusterSweep.R
@@ -1,10 +1,11 @@
-kClusterSweep <- function(nn_pct, ks_v = 10, seed_v = 123) {
+kClusterSweep <- function(nn_pct, ks_v = 10, seed_v = 123, verbose = F) {
   #' Sweep K for kmeans
   #' @description Test multiple k values for kmeans clustering
   #' @param nn_pct neighbor matrix converted to percentages
   #' @param ks_v either a single numeric value indicating to test k = 2 to k = ks_v;
   #' or a range of values to test.
   #' @param seed_v seed to set prior to running kmeans() for reproducibility. Will use same seed for each run of k.
+  #' @param verbose_v logical indicating whether or not to print message.
   #' @details Run kmeans clustering for specified values of k. Calculate mean silhouette width, mean RMSD (root mean-squared deviation),
   #' and within-cluster sum of squares. Plot silhouette and RMSD together and wcss as an elbow plot.
   #' @return prints 2 plots to console
@@ -13,32 +14,39 @@ kClusterSweep <- function(nn_pct, ks_v = 10, seed_v = 123) {
   
   ### Determine k
   if (length(ks_v) == 1) {
-    cat(sprintf("Single k value provided. Going to test values from 2 to %s.\n", ks_v))
+    if (verbose) cat(sprintf("Single k value provided. Going to test values from 2 to %s.\n", ks_v))
     ks_v <- 2:ks_v
   } else {
     cat(sprintf("Range of k values provided. Testing: %s\n", paste(ks_v, collapse = ' ')))
-    if (1 %in% ks_v) warning("Testing a k of 1 will not provide a silhouette score.")
+    if (verbose) if (1 %in% ks_v) warning("Testing a k of 1 will not provide a silhouette score.")
   } # fi
   
   ### Empty vector
   wcss <- vector("numeric", length(ks_v))
   meanSil <- vector("numeric", length(ks_v))
   meanRmsd <- vector("numeric", length(ks_v))
+  tocs_v <- NULL
   
   ### k means for each
   for (i in 1:length(ks_v)) {
     k <- ks_v[i]
     if (nrow(nn_pct) < 20) next  # careful this is arbitrary
     set.seed(seed_v)
+    tictoc::tic()
     kmeansMod <- kmeans(nn_pct, centers = k, nstart = 10, iter.max = 300)
     wcss[i] <- kmeansMod$tot.withinss
+    tocs_v <- c(tocs_v, tictoc::toc(quiet = T)$callback_msg)
     if (k == 1) {
       meanSil[i] <- NA
       meanRmsd[i] <- NA
     } else {
+      tictoc::tic()
       sil <- as.data.frame(bluster::approxSilhouette(x = nn_pct, clusters = unname(kmeansMod$cluster)))
+      tocs_v <- c(tocs_v, tictoc::toc(quiet = T)$callback_msg)
       meanSil[i] <- mean(sil$width)
+      tictoc::tic()
       rmsd <- clusterRMSD(x = nn_pct, clusters = unname(kmeansMod$cluster))
+      tocs_v <- c(tocs_v, tictoc::toc(quiet = T)$callback_msg)
       meanRmsd[i] <- mean(rmsd)
     } # fi
     
